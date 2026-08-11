71c7685d399a6f12eb457bb6013b8f033897a45d
steve-ongera <gadafisteve001@gmail.com>
2026-08-11 18:20:14 +0300

    made updates on backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 }
 
 
-AUTH_USER_MODEL = "mentalhealth.User"
+AUTH_USER_MODEL = "users.User"
  
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
